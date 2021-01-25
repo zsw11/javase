2eaa4a04355a280051038040bc3c89d62cc1ab80
zsw <546232194@qq.com>
2021-01-25 17:54:00 +0800

    java编程思想源码练习

--- src/com/bianchensixiang/collection/AlphabeticSearch.java
+++ src/com/bianchensixiang/collection/AlphabeticSearch.java
@@ -1,5 +1,8 @@
 package com.bianchensixiang.collection;
 
+import java.util.*;
+import net.mindview.util.*;
+
 /**
  * @author zsw
  * @date 2021/1/25 16:51
@@ -7,6 +10,12 @@ package com.bianchensixiang.collection;
  */
 public class AlphabeticSearch {
     public static void main(String[] args) {
+        String[] sa = Generated.array(new String[30], new RandomGenerator.String(5));
+        Arrays.sort(sa, String.CASE_INSENSITIVE_ORDER);
+        System.out.println(Arrays.toString(sa));
+        int index = Arrays.binarySearch(sa, sa[10],
+                String.CASE_INSENSITIVE_ORDER);
+        System.out.println("Index: "+ index + "\n"+ sa[index]);
 
     }
 }
---
+++ src/com/bianchensixiang/collection/ArrayOfGenerics.java
@@ -0,0 +1,16 @@
+package com.bianchensixiang.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author zsw
+ * @date 2021/1/25 17:02
+ * @description :
+ */
+public class ArrayOfGenerics {
+    public static void main(String[] args) {
+        List<String>[] ls;
+//        List[] la = new List(10);
+    }
+}
