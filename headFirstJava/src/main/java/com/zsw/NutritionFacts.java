b89addc8cf7a2fc2ff5473b103a48020038724c1
zsw <546232194@qq.com>
2021-02-07 10:44:39 +0800

    并发常用类，并发之间的通信

---
+++ headFirstJava/src/main/java/com/zsw/NutritionFacts.java
@@ -0,0 +1,15 @@
+//package com.zsw;
+//
+//import java.io.BufferedReader;
+//
+///**
+// * @author zsw
+// * @date 2021/2/2 14:22
+// * @description :
+// */
+//public class NutritionFacts {
+//    String firstLineOFFile(String path){
+//        BufferedReader bufferedReader = new BufferedReader(path);
+//
+//    }
+//}
