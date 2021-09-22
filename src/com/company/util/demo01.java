608318517fc53e40c7b72529b536f3cbf51fd961
shuwen.zhou <546232194@qq.com>
2021-09-22 09:44:59 +0800

    test

--- src/com/company/util/demo01.java
+++ src/com/company/util/demo01.java
@@ -3,6 +3,11 @@ package com.company.util;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.BeansException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author zsw
  * @date 2021/1/7 10:53
@@ -10,7 +15,8 @@ import org.springframework.beans.BeansException;
  */
 public class demo01 {
     public static void main(String[] args) {
-        beanTest();
+//        beanTest();
+        test();
 
     }
     public static void beanTest() throws BeansException {
@@ -27,5 +33,15 @@ public class demo01 {
 //        studentVo.setName("ztyj");
 //        student.setName("ztyj");
         System.out.println(student.toString()+"--------"+studentVo.toString());
+    }
+
+    public static void test() {
+        String s = "erre,wwww";
+        String s1 = s.substring(s.substring(0, s.indexOf(",")).length()+1);
+        List<Integer> existedIds = Arrays.asList(1, 2, 3);
+        List<Integer> validIds = Arrays.asList(1, 2, 3,4,5);
+        List<Integer> finalValidIds = validIds;
+        validIds = existedIds.stream().filter(a -> !finalValidIds.contains(a)).collect(Collectors.toList());
+        System.out.println(validIds);
     }
 }
