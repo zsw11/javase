85e0cfb684f398252f527b2e49e9bebc21f3c0ae
shuwen.zhou <546232194@qq.com>
2022-01-12 18:13:17 +0800

    优化

--- src/com/zsw_2020/data_2_21/SelectionSort.java
+++ src/com/zsw_2020/data_2_21/SelectionSort.java
@@ -13,16 +13,30 @@ public class SelectionSort {
      * @param array
      */
     public static void selectionSort(int[] array) {
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int min = i; //最小元素的下标
             for (int j = i + 1; j < array.length; j++) {
-                if (array[i] > array[j]) {
-                    int temp = array[j];
-                    array[j] = array[i];
-                    array[i] = temp;
+                if (array[j] < array[min]) {
+                    min = j;
                 }
-                Collections.singletonList(array).forEach(System.out::println);
+            }
+            if (min != i) {
+                swap(array, min, i);
             }
         }
+    }
+
+    /**
+     * 交换数组元素
+     *
+     * @param arr
+     * @param a
+     * @param b
+     */
+    public static void swap(int[] arr, int a, int b) {
+        arr[a] = arr[a] + arr[b];
+        arr[b] = arr[a] - arr[b];
+        arr[a] = arr[a] - arr[b];
     }
 
     /**
@@ -45,14 +59,15 @@ public class SelectionSort {
 
     /**
      * 插入排序
+     *
      * @param array
      */
-    public static void charupaixu(int [] array){
+    public static void charupaixu(int[] array) {
         for (int i = 1; i < array.length; i++) {
             for (int j = i; j > 0; j--) {
                 if (array[j] < array[j - 1]) {
                     int temp = array[j - 1];
-                    array[j-1] = array[j];
+                    array[j - 1] = array[j];
                     array[j] = temp;
                 }
             }
@@ -63,17 +78,18 @@ public class SelectionSort {
      * 快速排序是我们之前学习的冒泡排序的升级，他们都属于交换类排序，都是采用不断的比较和移动来实现排序的
      * 首先找一个基点，第一次通过比较，把基点大的数放到右边，比基点小的数放到左边。
      * 以基点为界限，基点右边和左边重复上面操作
+     *
      * @param args
      */
-    public static void kuaipai(){
+    public static void kuaipai() {
 
     }
 
     public static void main(String[] args) {
         int sorce[] = {34, 54, 23, 12, 34, 55};
-//        selectionSort(sorce);
+        selectionSort(sorce);
 //        maoSort(sorce);
-        charupaixu(sorce);
+//        charupaixu(sorce);
         for (int i : sorce) {
             System.out.println(i);
         }
